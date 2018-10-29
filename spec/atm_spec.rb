11eb83fe1d790ee10970969a1de1e4fdb41d74c1
camron <camronmoradi@gmail.com>
2018-10-29 20:47:10 +0100

    Update withdraw method with case: insufficient funds in account

--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -22,5 +22,9 @@ describe Atm do
         expect(subject.withdraw(45, account)).to eq expected_output
     end
 
+    it 'rejects withdraw if account has insufficient funds' do
+        expected_output = { status: false, message: 'insufficient funds', date: Date.today }
+        expect(subject.withdraw(105, account)).to eq expected_output
+    end
 
 end
