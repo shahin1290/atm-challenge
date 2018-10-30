6812de3b01af3df076bcd912773c236271c47724
camron <camronmoradi@gmail.com>
2018-10-30 21:00:31 +0100

    Validate account expiry date + fixed pin code validation

--- lib/account.rb
+++ lib/account.rb
@@ -1,6 +1,18 @@
 require 'date'
 
 class Account
+    attr_accessor :pin_code, :exp_date
+    
+    STANDARD_VALIDITY_YRS = 5
+
+    def initialize
+        @pin_code = rand(1000..9999)
+    end
+
+    def exp_date
+       Date.today.next_year(Account::STANDARD_VALIDITY_YRS).strftime('%m/%y')
+    end
+    
 end
 
-#    pin_code: '1234', exp_date: '04/19', account_status: :active, account owner
+# :pin_code, :balance,:account_statusand :exp_date, :account_owner
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -3,11 +3,13 @@ require './lib/account.rb'
 describe Account do
 
     it 'check length of a number' do
-        number = 1234
-        number_length = Math.log10(number).to_i + 1
+        number_length = Math.log10(subject.pin_code).to_i + 1
         expect(number_length).to eq 4
     end
 
-
+    it 'is expected to have an expiry date on initialize' do
+        expected_date = Date.today.next_year(5).strftime("%m/%y")
+        expect(subject.exp_date).to eq expected_date
+    end
 
 end
