0f04f02bcf7059ec648edfd17d6dd1837b200e5b
camron <camronmoradi@gmail.com>
2018-10-30 10:27:12 +0100

    Update withdraw method with case: incorrect pin

--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -6,11 +6,11 @@ describe Atm do
     end
 
     it 'funds are reduced at withdraw' do
-        subject.withdraw(50, account)
+        subject.withdraw(50, '1234', account)
         expect(subject.funds).to eq 950
     end
 
-    let(:account) { instance_double('Account')}
+    let(:account) { instance_double('Account', pin_code: '1234')}
 
     before do
         allow(account).to receive(:balance).and_return(100)
@@ -19,18 +19,23 @@ describe Atm do
 
     it 'allow withdraw if account has enough balance.' do
         expected_output = { status: true, message: 'success', date: Date.today, amount: 45 }
-        expect(subject.withdraw(45, account)).to eq expected_output
+        expect(subject.withdraw(45, '1234', account)).to eq expected_output
     end
 
     it 'rejects withdraw if account has insufficient funds' do
         expected_output = { status: false, message: 'insufficient funds', date: Date.today }
-        expect(subject.withdraw(105, account)).to eq expected_output
+        expect(subject.withdraw(105, '1234', account)).to eq expected_output
     end
 
     it 'reject withdraw if ATM has insufficient funds' do
         subject.funds = 50
         expected_output = { status: false, message: 'insufficient funds in ATM', date: Date.today }
-        expect(subject.withdraw(100, account)).to eq expected_output
-      end
+        expect(subject.withdraw(100, '1234', account)).to eq expected_output
+    end
+
+    it 'reject withdraw if pin is wrong' do
+        expected_output = {status: false, message: 'wrong pin', date: Date.today }
+        expect(subject.withdraw(50, 9999, account)).to eq expected_output
+    end
 
 end
